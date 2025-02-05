60aa8e21d52f519d548ec7039167b42280620886
nikunjlad <nikunjrlad@gmail.com>
2025-02-04 23:20:34 -0800

    adding optimal C++ solution for largest element in array

--- arrays/easy/cpp/largestElement.cpp
+++ arrays/easy/cpp/largestElement.cpp
@@ -8,21 +8,42 @@ Given an array of integers, find the largest element in the array.
 using namespace std;
 
 // BRUTE FORCE:
+// Time Complexity: O(nlogn)
+// Space Complexity: O(1)
+// int largestElement(vector<int> &arr, int n) {
+
+//     // sort the numbers in ascending order
+//     sort(arr.begin(), arr.end());
+
+//     return arr[n-1];
+
+// }
+
+
+// OPTIMAL:
+// Time Complexity: O(n)
+// Space Complexity: O(1)
 int largestElement(vector<int> &arr, int n) {
 
-    // sort the numbers in ascending order
-    sort(arr.begin(), arr.end());
+    int largest = arr[0];
 
-    return arr[n-1];
+    for(int i=0; i<n; i++) {
+        if(arr[i] > largest){
+            largest = arr[i];
+        }
+    }
+
+    return largest;
 
 }
+
 
 int main(){
     vector<int> arr1 = {1, 8, 7, 56, 90};
     vector<int> arr2 = {5, 5, 5, 5};
     vector<int> arr3 = {10};
 
-    cout << largestElement(arr3, 1) << endl;
+    cout << largestElement(arr1, 5) << endl;
     return 0;
 }
 
